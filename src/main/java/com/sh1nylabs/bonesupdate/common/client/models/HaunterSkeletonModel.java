b0f24193437bb97e804bd767190953e2ebd8a795
Sh1nyLabs <contact.shinylabs@gmail.com>
2024-01-08 23:16:06 +0100

    haunter model added

--- src/main/java/com/sh1nylabs/bonesupdate/common/client/models/HaunterSkeletonModel.java
+++ src/main/java/com/sh1nylabs/bonesupdate/common/client/models/HaunterSkeletonModel.java
@@ -8,7 +8,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.sh1nylabs.bonesupdate.BonesUpdate;
 import com.sh1nylabs.bonesupdate.common.entities.custom_skeletons.HaunterSkeleton;
-import com.sh1nylabs.bonesupdate.common.entities.custom_skeletons.KnightSkeleton;
 import net.minecraft.client.model.ArmedModel;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
@@ -32,44 +31,49 @@ public class HaunterSkeletonModel extends EntityModel<HaunterSkeleton> implement
 
 	public HaunterSkeletonModel(ModelPart root) {
 		this.head = root.getChild("head");
-		this.broken_state = root.getChild("broken_state");
 		this.body = root.getChild("body");
 		this.left_arm = root.getChild("left_arm");
 		this.right_arm = root.getChild("right_arm");
 		this.left_leg = root.getChild("left_leg");
 		this.right_leg = root.getChild("right_leg");
+		this.broken_state = root.getChild("broken_state");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F));
+		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-		PartDefinition claw = head.addOrReplaceChild("claw", CubeListBuilder.create().texOffs(0, 6).addBox(-5.0F, -2.5F, -4.5F, 10.0F, 3.0F, 6.0F, new CubeDeformation(0.0F))
-		.texOffs(3, 0).addBox(-5.0F, -4.5F, -1.5F, 10.0F, 2.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0873F, 0.0F, 0.0F));
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(8, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-		PartDefinition broken_state = partdefinition.addOrReplaceChild("broken_state", CubeListBuilder.create(), PartPose.offset(0.0F, 23.0F, 0.0F));
+		PartDefinition left_arm = partdefinition.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(32, 18).mirror().addBox(-1.0F, -2.0F, -1.0F, 2.0F, 12.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(41, 24).mirror().addBox(-1.001F, -3.0F, -2.0F, 3.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false)
+		.texOffs(41, 20).addBox(-1.0F, 7.0F, -1.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.5F)), PartPose.offset(5.0F, 2.0F, 0.0F));
 
-		PartDefinition head_r1 = broken_state.addOrReplaceChild("head_r1", CubeListBuilder.create().texOffs(32, 0).addBox(1.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 1.0F, 0.0F, 0.0F, -0.3927F, 0.0F));
-
-		PartDefinition body_r1 = broken_state.addOrReplaceChild("body_r1", CubeListBuilder.create().texOffs(11, 15).addBox(0.0F, -10.0F, -4.5F, 8.0F, 12.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-6.0F, 0.0F, -3.0F, -1.1961F, 0.2291F, -0.5236F));
-
-		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(11, 15).addBox(-4.0F, -12.0F, -2.5F, 8.0F, 12.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 12.0F, 0.0F));
-
-		PartDefinition left_arm = partdefinition.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(52, 17).mirror().addBox(0.0F, -2.0F, -1.5F, 3.0F, 12.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(4.0F, 2.0F, 0.0F));
-
-		PartDefinition right_arm = partdefinition.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(52, 17).addBox(-3.0F, -2.0F, -1.5F, 3.0F, 12.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(-4.0F, 2.0F, 0.0F));
+		PartDefinition right_arm = partdefinition.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(32, 18).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 12.0F, 2.0F, new CubeDeformation(0.0F))
+		.texOffs(41, 24).addBox(-1.999F, -3.0F, -2.0F, 3.0F, 4.0F, 4.0F, new CubeDeformation(0.0F))
+		.texOffs(41, 20).mirror().addBox(-1.0F, 7.0F, -1.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.5F)).mirror(false), PartPose.offset(-5.0F, 2.0F, 0.0F));
 
 		PartDefinition left_leg = partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 16).mirror().addBox(-1.0F, 0.0F, -1.1F, 2.0F, 12.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(2.0F, 12.0F, 0.1F));
 
 		PartDefinition right_leg = partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 16).addBox(-1.0F, 0.0F, -1.1F, 2.0F, 12.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 12.0F, 0.1F));
+
+		PartDefinition broken_state = partdefinition.addOrReplaceChild("broken_state", CubeListBuilder.create().texOffs(41, 24).mirror().addBox(3.999F, -4.0F, 3.0F, 3.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition body_r1 = broken_state.addOrReplaceChild("body_r1", CubeListBuilder.create().texOffs(8, 16).addBox(-4.0F, -6.0F, -5.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -1.4399F, 0.0F, 0.0F));
+
+		PartDefinition left_bracelet_r1 = broken_state.addOrReplaceChild("left_bracelet_r1", CubeListBuilder.create().texOffs(41, 20).addBox(4.0F, -1.0F, -4.0F, 2.0F, 1.0F, 2.0F, new CubeDeformation(0.5F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, -0.48F, 0.0F));
+
+		PartDefinition head_r1 = broken_state.addOrReplaceChild("head_r1", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-8.0F, 0.0F, 0.0F, 0.0F, -0.5672F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 32);
 	}
 
 	@Override
 	public void setupAnim(HaunterSkeleton entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		boolean flag2 = entity.isBroken();
+
 		this.right_leg.yRot = 0.005F;
 		this.left_leg.yRot = -0.005F;
 		this.right_leg.zRot = 0.005F;
@@ -88,12 +92,22 @@ public class HaunterSkeletonModel extends EntityModel<HaunterSkeleton> implement
 		}
 		float period=0.667F;
 
-		this.right_arm.x = -4.0F;
-		this.right_arm.y = 2.0F;
-		this.right_arm.z = 0.0F;
-		this.right_arm.xRot = Mth.cos(limbSwing * period + (float)Math.PI) * limbSwingAmount / f;
-		this.right_arm.yRot = 0.0F;
-		this.right_arm.zRot = 0.0F;
+		if (flag2) {
+			this.right_arm.x = -2.0F;
+			this.right_arm.y = -12.0F;
+			this.right_arm.z = -4.0F;
+			this.right_arm.xRot = -1;
+			this.right_arm.yRot = 0.0F;
+			this.right_arm.zRot = 0.0F;
+		} else {
+			this.right_arm.x = -4.0F;
+			this.right_arm.y = 2.0F;
+			this.right_arm.z = 0.0F;
+			this.right_arm.xRot = Mth.cos(limbSwing * period + (float)Math.PI) * limbSwingAmount / f;
+			this.right_arm.yRot = 0.0F;
+			this.right_arm.zRot = 0.0F;
+		}
+
 
 		this.left_arm.x = 4.0F;
 		this.left_arm.z = 0.0F;
@@ -119,7 +133,7 @@ public class HaunterSkeletonModel extends EntityModel<HaunterSkeleton> implement
 		this.left_leg.xRot = Mth.cos(limbSwing * period + (float)Math.PI) * 1.4F * limbSwingAmount / f;
 
 
-		boolean flag2 = entity.isBroken();
+
 		this.head.visible=!flag2;
 		this.body.visible=!flag2;
 		this.left_arm.visible=!flag2;
@@ -135,15 +149,14 @@ public class HaunterSkeletonModel extends EntityModel<HaunterSkeleton> implement
 		ModelPart armPart= (arm == HumanoidArm.LEFT) ? this.left_arm : this.right_arm;
 		armPart.translateAndRotate(stack);
 	}
-
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
 		head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-		broken_state.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		left_arm.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		right_arm.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		left_leg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		right_leg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		broken_state.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
--- src/main/java/com/sh1nylabs/bonesupdate/common/client/renderer/HaunterSkeletonRenderer.java
+++ src/main/java/com/sh1nylabs/bonesupdate/common/client/renderer/HaunterSkeletonRenderer.java
@@ -1,10 +1,8 @@
 package com.sh1nylabs.bonesupdate.common.client.renderer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.sh1nylabs.bonesupdate.BonesUpdate;
 import com.sh1nylabs.bonesupdate.common.client.models.HaunterSkeletonModel;
 import com.sh1nylabs.bonesupdate.common.entities.custom_skeletons.HaunterSkeleton;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
@@ -12,17 +10,11 @@ import net.minecraft.resources.ResourceLocation;
 
 public class HaunterSkeletonRenderer extends MobRenderer<HaunterSkeleton, HaunterSkeletonModel> {
 
-    public static final ResourceLocation TEXTURE=new ResourceLocation(BonesUpdate.MODID,"textures/entity/haunter.png");
+    public static final ResourceLocation TEXTURE=new ResourceLocation(BonesUpdate.MODID,"textures/entity/haunter_skeleton.png");
 
     public HaunterSkeletonRenderer(EntityRendererProvider.Context context) {
         super(context,new HaunterSkeletonModel(context.bakeLayer(HaunterSkeletonModel.LAYER_LOCATION)), 0.5f);
-        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()) {
-            public void render(PoseStack poseStack, MultiBufferSource source, int p_116354_, HaunterSkeleton haunter, float p_116356_, float p_116357_, float p_116358_, float p_116359_, float p_116360_, float p_116361_) {
-                if (!haunter.isBroken()) {
-                    super.render(poseStack, source, p_116354_, haunter, p_116356_, p_116357_, p_116358_, p_116359_, p_116360_, p_116361_);
-                }
-            }
-        });
+        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
     }
 
     public ResourceLocation getTextureLocation(HaunterSkeleton entity) {
